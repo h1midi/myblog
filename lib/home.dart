7c17165a17c9c7eaf9664986a3737a39d7912055
mohammed hamidi <mohammed.hamidi@univ-tlemcen.dz>
2021-04-11 22:28:16 +0100

    update#002

---
+++ lib/home.dart
@@ -0,0 +1,181 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/link.dart';
+
+class homeScreen extends StatefulWidget {
+  @override
+  _homeScreenState createState() => _homeScreenState();
+}
+
+class _homeScreenState extends State<homeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Link(
+            uri: Uri.parse("https://github.com/hammiddi"),
+            target: LinkTarget.blank,
+            builder: (context,folowLink){
+            return ElevatedButton(onPressed: folowLink, child: Text("GitHub"),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.transparent),
+                elevation: MaterialStateProperty.all<double>(0.0),
+              ),
+              );
+            }
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Link(
+            uri: Uri.parse("http://linkedin.com/in/hammiddi"),
+            target: LinkTarget.blank,
+            builder: (context,folowLink){
+            return ElevatedButton(onPressed: folowLink,
+             child: Text("LinkedIn"),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.transparent),
+                elevation: MaterialStateProperty.all<double>(0.0),
+              ),
+              );
+            }
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Link(
+            uri: Uri.parse("http://twitter.com/hammiddi"),
+            target: LinkTarget.blank,
+            builder: (context,folowLink){
+            return ElevatedButton(
+              onPressed: folowLink, 
+              child: Text("Twitter"),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.transparent),
+                elevation: MaterialStateProperty.all<double>(0.0),
+              ),
+              );
+            }
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Link(
+            uri: Uri.parse("mailto:mohammed.hamidi@univ-tlemcen.dz"),
+            target: LinkTarget.blank,
+            builder: (context,folowLink){
+            return ElevatedButton(
+              onPressed: folowLink, 
+              child: Text("Mail"),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.transparent),
+                elevation: MaterialStateProperty.all<double>(0.0),
+              ),
+              );
+            }
+          ),
+        ),
+        ],
+      ),
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Hamidi's"),
+            Text(
+              "Blog",
+              style: TextStyle(
+                      color: Colors.amber,
+                      fontWeight: FontWeight.w900,
+                    ),
+            )
+          ],
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+      ),
+      body:_content(context),
+
+    );
+  }
+}
+
+
+Widget _content(BuildContext context){
+  return Padding(
+    padding: EdgeInsets.all(16),
+    child: StreamBuilder(
+     stream:  FirebaseFirestore.instance.collection('blog').snapshots(),
+     builder: (BuildContext context, AsyncSnapshot snapshot){
+       switch (snapshot.connectionState ) {
+         
+         case ConnectionState.none:
+           return _error(context, 'no connection1');
+           break;
+         case ConnectionState.waiting:
+           return Center(child: CircularProgressIndicator(),);
+           break;
+         case ConnectionState.active:
+         case ConnectionState.done:
+            if (snapshot.hasError) {
+              print(snapshot.error.toString());
+              return _error(context, snapshot.error.toString());
+            }
+            if (! snapshot.hasData) {
+              return _error(context, 'no connection2');
+            }
+           return _drawScreen(context, snapshot.data);
+           break;
+       }
+     },
+      ),
+  );
+}
+
+Widget _drawScreen(BuildContext context,QuerySnapshot data) {
+  return ListView.builder(
+    itemCount: data.docs.length,
+    itemBuilder: (BuildContext context, int position){
+      return Card(
+              margin: EdgeInsets.all(16),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      data.docs[position]['title'],
+                      style: TextStyle(
+                        fontSize: 36,
+                        fontWeight: FontWeight.w600,
+                      ),
+                      textAlign: TextAlign.center,
+                     ),
+                     SizedBox(height: 12,),
+                    Text(
+                      data.docs[position]['desc'],
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.grey.shade300
+                      ),
+                      textAlign: TextAlign.center,
+                     ),
+                  ],
+                ),
+              ),
+              color: Colors.amber,
+              );
+  });
+}
+
+Widget _error(BuildContext context, String mssg){
+  return Center(
+     child: Text(mssg,style: TextStyle(color: Colors.amber),),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
-import 'dart:io';
-import 'package:url_launcher/link.dart';
+import 'package:myblog/home.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget { 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -29,163 +30,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Link(
-            uri: Uri.parse("https://github.com/hammiddi"),
-            target: LinkTarget.blank,
-            builder: (context,folowLink){
-            return ElevatedButton(onPressed: folowLink, child: Text("GitHub"),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(Colors.transparent),
-                elevation: MaterialStateProperty.all<double>(0.0),
-              ),
-              );
-            }
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Link(
-            uri: Uri.parse("http://linkedin.com/in/hammiddi"),
-            target: LinkTarget.blank,
-            builder: (context,folowLink){
-            return ElevatedButton(onPressed: folowLink,
-             child: Text("LinkedIn"),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(Colors.transparent),
-                elevation: MaterialStateProperty.all<double>(0.0),
-              ),
-              );
-            }
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Link(
-            uri: Uri.parse("http://twitter.com/hammiddi"),
-            target: LinkTarget.blank,
-            builder: (context,folowLink){
-            return ElevatedButton(
-              onPressed: folowLink, 
-              child: Text("Twitter"),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(Colors.transparent),
-                elevation: MaterialStateProperty.all<double>(0.0),
-              ),
-              );
-            }
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Link(
-            uri: Uri.parse("mailto:mohammed.hamidi@univ-tlemcen.dz"),
-            target: LinkTarget.blank,
-            builder: (context,folowLink){
-            return ElevatedButton(
-              onPressed: folowLink, 
-              child: Text("Mail"),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(Colors.transparent),
-                elevation: MaterialStateProperty.all<double>(0.0),
-              ),
-              );
-            }
-          ),
-        ),
-        ],
-      ),
-      appBar: AppBar(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Hamidi's"),
-            Text(
-              "Blog",
-              style: TextStyle(
-                      color: Colors.amber,
-                      fontWeight: FontWeight.w900,
-                    ),
-            )
-          ],
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.transparent,
-        elevation: 0.0,
-      ),
-      body: Center(
-        child: GridView.count(
-          padding: EdgeInsets.all(16),
-          crossAxisCount: 4,
-          children: [ 
-            Card(
-              margin: EdgeInsets.all(16),
-              child: Center(child: Text('Hello World')),
-              color: Colors.amber,
-              ),
-            Card(
-              margin: EdgeInsets.all(16),
-              child: Center(child: Text('Hello World')),
-              color: Colors.amber,
-              ),
-            Card(
-              margin: EdgeInsets.all(16),
-              child: Center(child: Text('Hello World')),
-              color: Colors.amber,
-              ),
-            Card(
-              margin: EdgeInsets.all(16),
-              child: Center(child: Text('Hello World')),
-              color: Colors.amber,
-              ),
-            Card(
-              margin: EdgeInsets.all(16),
-              child: Center(child: Text('Hello World')),
-              color: Colors.amber,
-              ),
-            Card(
-              margin: EdgeInsets.all(16),
-              child: Center(child: Text('Hello World')),
-              color: Colors.amber,
-              ),
-            Card(
-              margin: EdgeInsets.all(16),
-              child: Center(child: Text('Hello World')),
-              color: Colors.amber,
-              ),
-            Card(
-              margin: EdgeInsets.all(16),
-              child: Center(child: Text('Hello World')),
-              color: Colors.amber,
-              ),
-            Card(
-              margin: EdgeInsets.all(16),
-              child: Center(child: Text('Hello World')),
-              color: Colors.amber,
-              ),
-            Card(
-              margin: EdgeInsets.all(16),
-              child: Center(child: Text('Hello World')),
-              color: Colors.amber,
-              ),
-            Card(
-              margin: EdgeInsets.all(16),
-              child: Center(child: Text('Hello World')),
-              color: Colors.amber,
-              ),
-            Card(
-              margin: EdgeInsets.all(16),
-              child: Center(child: Text('Hello World')),
-              color: Colors.amber,
-              ),
-            ]
-          ),
-        ),
-        );
+    return homeScreen();
   }
 }
